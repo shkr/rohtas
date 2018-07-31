cec00174916e8503b96c39a2296fc127dd9be4dc
shashank <shashank.f1@gmail.com>
2018-07-31 13:55:13 -0700

    books updated

--- books/data.js
+++ books/data.js
@@ -1,15 +1,15 @@
-var NUM_BOOKS = 8;
+var NUM_BOOKS = 14;
 var BOOKS = {
 "0": {
     "title": "Nonlinear dynamics and Chaos",
     "image": "books/images/116164.jpg",
-    "genre": "Systems-Engineering",
+    "genre": "Systems",
     "url": "https://www.goodreads.com/book/show/116164.Nonlinear_Dynamics_and_Chaos"
   },
 "1": {
     "title": "The Web of Life: A New Scientific Understanding of Living Systems",
     "image": "books/images/314114.jpg",
-    "genre": "Systems-Engineering,Biology",
+    "genre": "Systems,Probability,Biology",
     "url": "https://www.goodreads.com/book/show/314114.The_Web_of_Life"
   },
 "2": {
@@ -47,5 +47,41 @@ var BOOKS = {
     "image": "books/images/17717.jpg",
     "genre": "Fiction,World",
     "url": "https://www.goodreads.com/book/show/17717.Labyrinths"
+  },
+"8": {
+    "title": "American Colonies: The Settling of North America",
+    "image": "books/images/95225.jpg",
+    "genre": "History,History>North&CentralAmerica",
+    "url": "https://www.goodreads.com/book/show/95225.American_Colonies"
+  },
+"9": {
+    "title": "Brighter than a Thousand Suns: A Personal History of the Atomic Scientists",
+    "image": "books/images/407305.jpg",
+    "genre": "History,History>North&CentralAmerica,History>Europe",
+    "url": "https://www.goodreads.com/book/show/407305.Brighter_than_a_Thousand_Suns"
+  },
+"10": {
+    "title": "The Orestia",
+    "image": "books/images/159869.jpg",
+    "genre": "Fiction,Culture>Greece",
+    "url": "https://www.goodreads.com/book/show/159869.The_Orestia"
+  },
+"11": {
+    "title": "Desert Solitaire",
+    "image": "books/images/17717.jpg",
+    "genre": "Fiction,Geography>Utah",
+    "url": "https://www.goodreads.com/book/show/17717.Labyrinths"
+  },
+"12": {
+    "title": "The End of History and the Last Man",
+    "image": "books/images/57981.jpg",
+    "genre": "History,History>World",
+    "url": "https://www.goodreads.com/book/show/57981.The_End_of_History_and_the_Last_Man"
+  },
+"13": {
+    "title": "Persepolis: The Story of a Childhood",
+    "image": "books/images/9516.jpg",
+    "genre": "Biography,Biography>Iran",
+    "url": "https://www.goodreads.com/book/show/9516.Persepolis"
   }
 }
